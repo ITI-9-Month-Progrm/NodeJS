cc640f11b0dbc0164abb7931d233012c0dc01075
unknown <asmaakhaled603@gmail.com>
2021-02-23 15:35:08 +0200

    Add Bonus Task

--- Day01/Task2/home.js
+++ Day01/Task2/home.js
@@ -1,12 +1,15 @@
 var http = require("http");
 var fs = require("fs");
+
 var port = 5000;
 var hostName = '127.0.0.1';
 var customServer  = http.createServer(function(request,response){
     console.log("request recieved");
-           let urlLastPArt = request.url;
-           switch(urlLastPArt){
-               case '/users':{
+           let url = request.url;
+           let urlParts = url.split("/");
+           
+           switch(urlParts[1]){
+               case 'users':{
                     response.statusCode=200;
                     response.setHeader('Content-type','text/plane');
                     let users = [];
@@ -30,7 +33,7 @@ var customServer  = http.createServer(function(request,response){
                     });
                    break;
                }
-               case '/background':{
+               case 'background':{
                 response.statusCode=200;
                 response.setHeader('Content-type','text/html');
                 
@@ -39,9 +42,6 @@ var customServer  = http.createServer(function(request,response){
                         console.log("something Error during read file")
                     }
                     else{
-                      
-
-                      //console.log(data.toString());
                       response.write(data);
                      
                       response.end();
@@ -50,29 +50,48 @@ var customServer  = http.createServer(function(request,response){
                 });
                    break;
                }
-               case '/image':
+               case 'image':
         fs.readFile('../Task2/images/profilePic.png',function(err,image){
             response.statusCode=200;
             response.setHeader('Content-type','image/png');
             response.end(image);
         })
         break;
-        case '/style':
+        case 'style':
             fs.readFile('background.css',function(err,data){
                 response.statusCode=200;
                 response.setHeader('Content-type','text/css');
                 response.end(data);
             })
             break;
+        //Bonus Task    
+        case 'getUser':
+            response.statusCode=200;
+            response.setHeader('Content-type','text/plane');
+            let users = [];
+            fs.readFile("../Task1/user.txt",(err,data)=>{
+                if(err){
+                    console.log("something Error during read file")
+                }
+                else{
+                  
+                  users=JSON.parse( data)
+                  for(let i=0;i<users.length;i++){
+                      if(users[i]["name"] == urlParts[2])
+                      {
+                        response.write(JSON.stringify(users[i]));
+                        break;
+                      }
+                  }
+                  response.end();
+                
+                }
+            });    
+             break;
         default:
             response.statusCode=404;
             response.setHeader('Content-type','text/plain');
             response.end('not found');
             break;
            }
-   
-            
-           
-
-
 }).listen(port,hostName);
